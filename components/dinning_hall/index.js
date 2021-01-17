e79dbaf6065d2a88eb68f5857edae239f6086417
Michael Equi <equim20@student.jhs.net>
2021-01-17 13:25:07 -0800

    Added stream

--- components/dinning_hall/index.js
+++ components/dinning_hall/index.js
@@ -1,10 +1,30 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
 
 import {MyContext} from "../../context";
+import AppLoading from "expo-app-loading";
 
 const DinningHall = ({route, navigation}) => {
   const context = useContext(MyContext);
+  const db = context.dbh;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const doc = db.collection("streams").doc(route.params.streamid);
+    const unsubscribe = doc.onSnapshot(snap => {
+      setData(snap.data());
+    });
+    return () => unsubscribe()
+  }, [db]);
+
+  if(data === null){
+    return (
+      <AppLoading/>
+    )
+  }
+
+  console.log(data);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Dinning Hall</Text>
