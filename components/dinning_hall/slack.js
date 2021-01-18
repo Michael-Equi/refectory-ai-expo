8d8c943d6a8bba569c5ef95e829ddc2d3c921dfc
Michael Equi <equim20@student.jhs.net>
2021-01-17 22:42:23 -0800

    Added keyboard dismissal

--- components/dinning_hall/slack.js
+++ components/dinning_hall/slack.js
@@ -1,5 +1,6 @@
 import React, {useRef} from 'react';
-import {View, Text, TextInput, SafeAreaView, TouchableOpacity} from 'react-native';
+import {View, Text, TextInput, SafeAreaView, TouchableOpacity, TouchableWithoutFeedback} from 'react-native';
+import {Keyboard} from 'react-native'
 import Clipboard from 'expo-clipboard';
 import styles from "../../styles";
 import * as colors from "../../styles/colors";
@@ -10,19 +11,21 @@ const Slack = ({route, navigation}) => {
   const string = "May I have the following meal saved?\n" + "\n" + items.map((item) => (` -> ${item}\n`))  + "\n" + "Thanks!\n" + "\n" + "* sent with Refectory";
 
   return (
-    <SafeAreaView style={{...styles.centered, flex: 1, justifyContent: 'space-between'}}>
-      <TextInput ref={textInput} multiline numberOfLines={10} style={styles.textInput}>
-        {string}
-      </TextInput>
-      <View>
-        <TouchableOpacity style={{...styles.button, marginBottom: 10}} title={"Copy"} onPress={() => {Clipboard.setString(string)}}>
-          <Text style={{...styles.boldBody, color: 'white'}}>Copy</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{...styles.button, marginTop: 10, borderColor: colors.primary, borderWidth: 2, backgroundColor: colors.secondary}} title={"Back"} onPress={() => {navigation.pop()}}>
-          <Text style={{...styles.boldBody, color: 'black'}}>Back</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <SafeAreaView style={{...styles.centered, flex: 1, justifyContent: 'space-between'}}>
+        <TextInput ref={textInput} multiline numberOfLines={10} style={styles.textInput}>
+          {string}
+        </TextInput>
+        <View>
+          <TouchableOpacity style={{...styles.button, marginBottom: 10}} title={"Copy"} onPress={() => {Clipboard.setString(string)}}>
+            <Text style={{...styles.boldBody, color: 'white'}}>Copy</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{...styles.button, marginTop: 10, borderColor: colors.primary, borderWidth: 2, backgroundColor: colors.secondary}} title={"Back"} onPress={() => {navigation.pop()}}>
+            <Text style={{...styles.boldBody, color: 'black'}}>Back</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   )
 }
 
